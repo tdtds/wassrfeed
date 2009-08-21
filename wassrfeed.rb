28e229dee084208afc649715bbb56ca13b40f00c
TADA Tadashi <sho@usuda.tdtds.jp>
2009-08-21 20:19:47 +0900

    librarized.

--- wassrfeed.rb
+++ wassrfeed.rb
@@ -34,49 +34,57 @@ module WassrFeedRC
 	end
 end
 
-def post_wassr( status )
-	wassr = Pit::get( 'wassr', :require => {
-		'user' => 'your ID of Wassr.',
-		'pass' => 'your Password of Wassr.',
-	} )
-	px_host, px_port = (ENV['http_proxy']||'').scan( %r|^(?:.*?)://(.*?):(\d+)?| ).flatten
-	timeout( 10 ) do
-		Net::HTTP.version_1_2
-		req = Net::HTTP::Post.new( '/statuses/update.json' )
-		req.basic_auth( wassr['user'], wassr['pass'] )
-		req.body = "source=wassrfeed&status=#{status}"
-		Net::HTTP::Proxy( px_host, px_port ).start( 'api.wassr.jp', 80 ) do |http|
-			res = http.request( req )
+class Wassr
+	def initialize
+		@login = Pit::get( 'wassr', :require => {
+			'user' => 'your ID of Wassr.',
+			'pass' => 'your Password of Wassr.',
+		} )
+	end
+
+	def post_status( status )
+		px_host, px_port = (ENV['http_proxy']||'').scan( %r|^(?:.*?)://(.*?):(\d+)?| ).flatten
+		timeout( 10 ) do
+			Net::HTTP.version_1_2
+			req = Net::HTTP::Post.new( '/statuses/update.json' )
+			req.basic_auth( @login['user'], @login['pass'] )
+			req.body = "source=wassrfeed&status=#{status}"
+			Net::HTTP::Proxy( px_host, px_port ).start( 'api.wassr.jp', 80 ) do |http|
+				res = http.request( req )
+			end
 		end
 	end
 end
 
-conf = WassrFeedRC::load
-
-while uri = ARGV.shift
-	# reading feed
-	feed = RSS::Parser::parse( open( uri, &:read ) )
-
-	# reading latest status
-	latest = conf[:latest][feed.channel.link] || Time::now
-
-	# making status list
-	status = []
-	feed.channel.items.each do |item|
-		status << item if item.pubDate > latest
-	end
-
-	status.sort_by{|i| i.pubDate}.each do |item|
-		# trimming ID when twitter
-		if %r|^http://twitter\.com/| =~ item.link then
-			item.description.sub!( /^[^:]*: /, '' )
-			next if item.description.include?( '@' )
+if __FILE__ == $0 then
+	conf = WassrFeedRC::load
+	
+	while uri = ARGV.shift
+		# reading feed
+		feed = RSS::Parser::parse( open( uri, &:read ) )
+	
+		# reading latest status
+		latest = conf[:latest][feed.channel.link] || Time::now
+	
+		# making status list
+		status = []
+		feed.channel.items.each do |item|
+			status << item if item.pubDate > latest
 		end
-
-		post_wassr item.description
-		conf[:latest][feed.channel.link] = item.pubDate
+	
+		wassr = Wassr::new
+		status.sort_by{|i| i.pubDate}.each do |item|
+			# trimming ID when twitter
+			if %r|^http://twitter\.com/| =~ item.link then
+				item.description.sub!( /^[^:]*: /, '' )
+				next if item.description.include?( '@' )
+			end
+	
+			wassr.post_status( item.description )
+			conf[:latest][feed.channel.link] = item.pubDate
+			WassrFeedRC.save( conf )
+			sleep( 1 )
+		end
 		WassrFeedRC.save( conf )
-		sleep( 1 )
 	end
-	WassrFeedRC.save( conf )
 end
