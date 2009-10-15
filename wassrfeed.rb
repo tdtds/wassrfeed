a41cf4c83c3074ae711855dd6b467ab0281ddac9
TADA Tadashi <t@tdtds.jp>
2009-10-15 18:23:45 +0900

    splited post method from post_status method in Wassr class.

--- wassrfeed.rb
+++ wassrfeed.rb
@@ -44,12 +44,18 @@ class Wassr
 	end
 
 	def post_status( status )
+		params = "source=wassrfeed&status=#{CGI::escape status}"
+		post( '/statuses/update.json', params )
+	end
+
+	:private
+	def post( end_point, params )
 		px_host, px_port = (ENV['http_proxy']||'').scan( %r|^(?:.*?)://(.*?):(\d+)?| ).flatten
 		timeout( 10 ) do
 			Net::HTTP.version_1_2
-			req = Net::HTTP::Post.new( '/statuses/update.json' )
+			req = Net::HTTP::Post.new( end_point )
 			req.basic_auth( @login['user'], @login['pass'] )
-			req.body = "source=wassrfeed&status=#{CGI::escape status}"
+			req.body = params
 			Net::HTTP::Proxy( px_host, px_port ).start( 'api.wassr.jp', 80 ) do |http|
 				res = http.request( req )
 			end
